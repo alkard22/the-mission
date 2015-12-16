ab03344f9b2f4deb2cd49022e83e89c3939b1f94
Joh Robbins <joh.robbins@rea-group.com>
2015-12-16 20:20:14 +1100

    Population text rotates towards the players direction

---
+++ TheMission/Assets/Scripts/GJPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GJPlayer : MonoBehaviour
+{
+    #region members
+    public static GJPlayer Instance = null;
+    #endregion
+
+    #region mono functions
+    void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+}
---
+++ TheMission/Assets/Scripts/GJRotateTowards.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GJRotateTowards : MonoBehaviour
+{
+    #region members
+    public GameObject m_target = null;
+    #endregion
+
+    #region mono functions
+
+    void Start () 
+    {
+		if (m_target == null)
+			m_target = GJPlayer.Instance.gameObject;
+	}
+
+	void Update () 
+	{
+		if (m_target == null)
+			m_target = GJPlayer.Instance.gameObject;
+
+		Vector3 targetPosition = -m_target.transform.position;
+		targetPosition.y = transform.position.y;
+
+		transform.LookAt(targetPosition);
+	}
+    #endregion
+}
